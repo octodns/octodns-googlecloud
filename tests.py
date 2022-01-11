61f60415e4e0be98e65eb3dc96b8c09871fb15ef
Ross McFarland <rwmcfa1@gmail.com>
2022-01-11 12:27:24 -0800

    Import skeleton

---
+++ tests.py
@@ -0,0 +1,12 @@
+#
+#
+#
+
+from unittest import TestCase
+
+from octodns_googlecloud import GoogleCloudProvider
+
+
+class TestGoogleCloudProvider(TestCase):
+    # TODO: test provider
+    GoogleCloudProvider
